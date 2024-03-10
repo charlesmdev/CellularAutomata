00ca223325b6d8370e9489ee9648ecee45b19815
CharlesM <charlesvincent.manaois@sjsu.edu>
2024-03-10 16:18:06 -0700

    Added ControlPanel nested class

--- src/mvc/AppPanel.java
+++ src/mvc/AppPanel.java
@@ -1,6 +1,7 @@
 package mvc;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -19,5 +20,20 @@ public class AppPanel extends JPanel implements ActionListener, Subscriber {
     @Override
     public void update() {
 
+    }
+    //Adds ControlPanel and buttons
+    public class ControlPanel extends JPanel {
+
+        public ControlPanel() {
+            setBackground(Color.LIGHT_GRAY);
+            setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        }
+        public void addButton(JButton button) {
+            JPanel buttonPanel = new JPanel();
+            //button.addActionListener(AppPanel.this); //Maybe not needed
+            buttonPanel.add(button);
+            add(button);
+
+        }
     }
 }
