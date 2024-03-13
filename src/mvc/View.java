b90e3d61d62b310b52118d07a2736aafdeee74d3
chsaad98 <143139058+chsaad98@users.noreply.github.com>
2024-03-12 17:06:47 -0700

    View.java

--- src/mvc/View.java
+++ src/mvc/View.java
@@ -1,28 +1,32 @@
 package mvc;
 
-import javax.swing.*;
+import javax.swing.JPanel;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
-public class View extends JPanel implements Subscriber, PropertyChangeListener {
+public abstract class View extends JPanel implements PropertyChangeListener {
+    protected Model model;
 
-    Model model;
+    public View(Model model) {
+        setModel(model);
+    }
 
     public void setModel(Model model) {
+        if (this.model != null) {
+            this.model.removePropertyChangeListener(this);
+        }
         this.model = model;
+        if (this.model != null) {
+            this.model.addPropertyChangeListener(this);
+        }
+        updateView();
     }
 
-    @Override
-    public void update() {
-
-    }
-
-    public void makeView(Model model) {
-        this.model = model;
-    }
-
-    @Override
     public void propertyChange(PropertyChangeEvent evt) {
-
+        if (evt.getSource() == model) {
+            updateView();
+        }
     }
+
+    protected abstract void updateView();
 }
