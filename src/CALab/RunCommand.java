49992f333b839963c2ef1a0ada4e74279af693af
chsaad98 <143139058+chsaad98@users.noreply.github.com>
2024-03-16 16:51:43 -0700

    Update RunCommand.java

--- src/CALab/RunCommand.java
+++ src/CALab/RunCommand.java
@@ -1,15 +1,1 @@
-package CALab;
-
-import mvc.*;
-
-public class RunCommand extends Command {
-
-    public RunCommand(Model model) {
-        super(model);
-    }
-
-    public void execute() {
-        ((Grid) model).updateLoop(1); 
-    }
-}
 
